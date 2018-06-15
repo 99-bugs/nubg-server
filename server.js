488cceeaabf3c9aa1194f927d8e2f27833f8fc36
Nico De Witte <nico.dewitte@vives.be>
2018-06-15 11:26:36 +0200

    Add mqtt async package

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ var duke = new Tank("Tha DUKE", sille);
 console.log(littletank);
 console.log(duke);
 
+
+
+var MQTT = require("async-mqtt");
+var AsyncClient = MQTT.AsyncClient;
+var client = new AsyncClient(MQTT.connect("tcp://mqtt.labict.be:1883"));
+ 
+
+client.publish("test/nubg", "server comming online").then(function(){
+    console.log("We async now");
+    return client.end();
+});
+
+
+
 var game = new Game();
 console.log(game);
 
