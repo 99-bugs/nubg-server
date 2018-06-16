12c3cbc2a20660d38cd43a97bc9cc59e097aba16
Nico De Witte <nico.dewitte@outlook.com>
2018-06-16 08:10:04 +0200

    Add logging using winston and simple mqtt test

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+var winston = require('winston');
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console()
+    // new winston.transports.File({ filename: 'combined.log' })
+  ], level: 'debug'
+});
+
 var Tank = require('./lib/tank');
 var Player = require('./lib/player');
-var Game = require('./lib/match');
+var Game = require('./lib/game');
 
 var bio = new Player("BioBoost");
 var littletank = new Tank("My Little Tank", bio);
@@ -11,20 +19,23 @@ var duke = new Tank("Tha DUKE", sille);
 console.log(littletank);
 console.log(duke);
 
-
-
-var MQTT = require("async-mqtt");
-var AsyncClient = MQTT.AsyncClient;
-var client = new AsyncClient(MQTT.connect("tcp://mqtt.labict.be:1883"));
- 
-
-client.publish("test/nubg", "server comming online").then(function(){
-    console.log("We async now");
-    return client.end();
-});
-
-
-
 var game = new Game();
 console.log(game);
 
+var mqtt = require('mqtt');
+var client = mqtt.connect("tcp://mqtt.labict.be:1883");
+ 
+client.on('connect', function () {
+  logger.info("Connected to broker");
+  client.subscribe('test/nubg/general');
+  client.publish('test/nubg/gamestate', JSON.stringify({msg: "imagine this to be game state"}));
+});
+ 
+client.on('message', function (topic, message) {
+  logger.debug("Received mqtt message '" + message.toString() + "' @" + topic);
+  console.log(message.toString());
+  client.end();
+});
+
+
+// game.stop();
