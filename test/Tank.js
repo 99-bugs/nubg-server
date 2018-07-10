4089f79768916c1ba94dac9df25441214f3ab054
Sille Van Landschoot <info@sillevl.be>
2018-07-10 18:10:54 +0200

    refactor Tank and add tests

---
+++ test/Tank.js
@@ -0,0 +1,44 @@
+let assert = require('assert');
+let Tank = require('../lib/Tank');
+
+
+describe('Tank', () => {
+  it('has a name',  () => {
+    let name = 'John';
+    let tank = new Tank(name);
+    assert.equal(tank.name, name);
+  });
+  
+  it('has an id that depends on the name', () => {
+    let tank = new Tank('John');
+    assert.equal(tank.id, "5753a498f025464d72e088a9d5d6e872592d5f91");
+  })
+
+  it('has a default location of 0,0', () => {
+    let tank = new Tank("John");
+    assert.equal(tank.location.x, 0);
+    assert.equal(tank.location.y, 0);
+  });
+
+  it('has an optional location on create', () => {
+    let tank = new Tank("John", {location: {x: 100, y: 200}});
+    assert.equal(tank.location.x, 100);
+    assert.equal(tank.location.y, 200);
+  })
+
+  it('has a default direction of 0', () => {
+    let tank = new Tank("John");
+    assert.equal(tank.direction, 0);
+  });
+
+  it('has an optional direction on create', () => {
+    let tank = new Tank("John", {direction: 180});
+    assert.equal(tank.direction, 180);
+  })
+
+  it('has a default health power of 100', () => {
+    let tank = new Tank("John");
+    assert.equal(tank.hp, 100);
+  });
+
+});
